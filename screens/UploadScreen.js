4938bdd48098779694d006937a16f6191fb49738
ziuwuchuoi <vv0320nt@gmail.com>
2023-10-10 15:02:42 +0700

    oke nhóe

--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -8,80 +8,51 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import scale from '../src/constants/responsive';
-import {IMG_LOGO} from '../src/assets/images';
-import {IC_UPLOAD} from '../src/assets/icons';
-import {useNavigation} from '@react-navigation/native';
+import { IMG_LOGO } from '../src/assets/images';
+import { IC_UPLOAD } from '../src/assets/icons';
+import { useNavigation } from '@react-navigation/native';
 import DocumentPicker from 'react-native-document-picker';
 //import storage from '@react-native-firebase/storage'
-import {PermissionsAndroid} from 'react-native';
+import { PermissionsAndroid } from 'react-native';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import axios from 'axios';
 import FormData from 'form-data';
 
-export const UploadScreen = ({props}) => {
+export const UploadScreen = ({ props }) => {
   const navigation = useNavigation();
-
+  const fs = require('react-native-fs');
   const [file, setFile] = useState('');
   async function pickDocument() {
     try {
-      // let index = 0
-      const result = await DocumentPicker.pick({
-        type: [DocumentPicker.types.allFiles],
-      });
-
-      // const newResult = result.map(item =>({
-      //   ...item,
-      //   key: index.toString()
-      //   }))
-
-      //   console.log(newResult)
-      // setDocuments(prevData => [
-      //   ...prevData,
-      //   result[0]
-      // ]);
-      setFile(result[0]);
-
-      // index++;
-
-      console.log(result[0]);
-      // navigation.navigate("Starting")
+      const result = await launchCamera({});
+      console.log(result)
+      setFile(result.assets[0].uri)
     } catch (err) {
-      if (DocumentPicker.isCancel(err)) {
-        // User cancelled the picker
-      } else {
-        throw err;
-      }
+
     }
   }
 
-  const fs = require('react-native-fs');
   async function handleSubmit() {
+    // navigation.navigate('Starting', { item: file, text:"asasdr" },
+    //   );
     let data = new FormData();
-    data.append('my_image', file);
-
+    console.log(file);
+    data.append('my_image', {
+      uri: file,
+      name: 'image',
+      type: 'image/jpeg' // or your mime type what you want
+    });
     let config = {
       method: 'post',
       maxBodyLength: Infinity,
       url: 'https://bichhang21522041-clip.hf.space/submit',
-      headers: {'Content-Type': 'multipart/form-data'},
+      headers: { 'Content-Type': 'multipart/form-data' },
       data: data,
     };
-    try {
-      navigation.navigate('Starting', {
-        params: {item: file.uri},
-      })
-      const response = await axios(config);
-      console.log(response.data[0]);
-    } catch (error) {
-      console.error(error);
-    }
-    // axios.request(config)
-    // .then((response) => {
-    //   console.log(data)
-    //   console.log(JSON.stringify(response.data));
-    // })
-    // .catch((error) => {
-    //   console.log(error);
-    // });
+    await axios(config).then((response) => {
+      navigation.navigate('Starting', { item: file, text: response.data[0] },
+      );
+    })
   }
   return (
     <SafeAreaView style={styles.container}>
@@ -91,28 +62,25 @@ export const UploadScreen = ({props}) => {
         </View>
         <View style={styles.titleContainer}>
           <Text style={styles.appName}>C L I P</Text>
-          <Text style={styles.appTitle}>I M A G E C L A S S I F I E R</Text>
+          <Text style={styles.appTitle}>I  M  A  G  E  C  L  A  S  S  I  F  I  E  R</Text>
         </View>
       </View>
       <View style={styles.bottomContainer}>
         <Text style={styles.reviewTitle}>
           This app will predict {'\n'}the class based on your image!
         </Text>
-        <Text style={styles.fileName}>{file.name}</Text>
+        <Text style={styles.fileName}>{file}</Text>
         <TouchableOpacity
           style={styles.buttonContainer}
           onPress={() => pickDocument()}>
           <Image source={IC_UPLOAD}></Image>
           <Text style={styles.buttonText}>Upload files</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.submitContainer} onPress={() => handleSubmit()}>
-          <Text style={styles.submitText} >
-            Submit
-          </Text>
+        <TouchableOpacity
+          style={styles.submitContainer}
+          onPress={() => handleSubmit()}>
+          <Text style={styles.submitText}>Submit</Text>
         </TouchableOpacity>
-        {/* <Text style={styles.note}>
-          Upload image file for the app to predict the class name.
-        </Text> */}
       </View>
     </SafeAreaView>
   );
@@ -146,15 +114,15 @@ const styles = StyleSheet.create({
   },
   bottomContainer: {
     width: '100%',
-    flex: 0.4,
+    flex: 0.5,
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40,
     backgroundColor: '#FFA500',
   },
   reviewTitle: {
     textAlign: 'center',
-    marginTop: scale(36, 'h'),
-    fontSize: scale(25, 'h'),
+    marginTop: scale(35, 'h'),
+    fontSize: scale(23, 'h'),
     fontWeight: 'bold',
     color: 'white',
   },
@@ -187,7 +155,10 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     alignSelf: 'center',
     marginTop: scale(15, 'h'),
-    width: "80%"
+    width: '80%',
+    overflow: 'hidden',
+    flexWrap: 'nowrap',
+    textDecorationStyle: 'dotted',
   },
   submitContainer: {
     width: scale(199, 'w'),
@@ -198,13 +169,12 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     alignSelf: 'center',
     marginTop: scale(20, 'h'),
-
   },
   submitText: {
     color: 'orange',
     fontSize: scale(20, 'h'),
     fontWeight: 'bold',
-  }
+  },
 });
 
 export default UploadScreen;
